19842e12f85e4da4d6a0b92c46961939936de4f7
ahmed <ahmed.sherif.fci@gmail.com>
2020-08-26 19:08:06 +0200

    add basic navbar

---
+++ ECommerce.App/ViewModels/Shared/NavbarViewModel.cs
@@ -0,0 +1,23 @@
+﻿using ECommerce.Core;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Main.ViewModels.Shared
+{
+    public class NavbarViewModel: BindableBase
+    {
+        private string _username;
+        public string Username
+        {
+            get { return _username; }
+            set { SetProperty(ref _username, value); }
+        }
+
+        public NavbarViewModel()
+        {
+            Username = Global.UserName.ToString();
+        }
+    }
+}
---
+++ ECommerce.App/Views/Shared/NavbarView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ECommerce.Main.Views.Shared
+{
+    /// <summary>
+    /// Interaction logic for NavbarView.xaml
+    /// </summary>
+    public partial class NavbarView : UserControl
+    {
+        public NavbarView()
+        {
+            InitializeComponent();
+        }
+    }
+}
