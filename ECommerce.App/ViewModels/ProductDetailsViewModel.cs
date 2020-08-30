06b78439d2a3495ffba543810bbb851cc9d85177
ahmed <ahmed.sherif.fci@gmail.com>
2020-08-30 18:21:08 +0200

    Product Details Screen - basic design

--- ECommerce.App/ViewModels/ProductDetailsViewModel.cs
+++ ECommerce.App/ViewModels/ProductDetailsViewModel.cs
@@ -1,19 +1,71 @@
-﻿using Prism.Mvvm;
+﻿using ECommerce.Core.Constants;
+using ECommerce.Main.Models;
+using ECommerce.Main.Services;
+using Prism.Commands;
+using Prism.Mvvm;
 using Prism.Regions;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ECommerce.Main.ViewModels
 {
     public class ProductDetailsViewModel: BindableBase, INavigationAware
     {
-        private int _productId;
-        public int ProductId
+        private Product product;
+        private readonly IProductService _productService;
+        private readonly IRegionManager _regionManager;
+
+        public Product Product
         {
-            get { return _productId; }
-            set { SetProperty(ref _productId, value); }
+            get { return product; }
+            set { SetProperty(ref product, value); }
         }
+
+        private int _stepper = 1;
+        public int Stepper
+        {
+            get { return _stepper; }
+            set 
+            { 
+                SetProperty(ref _stepper, value);
+                DecreaseStepperCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public ProductDetailsViewModel(IProductService productService, IRegionManager regionManager)
+        {
+            _productService = productService;
+            _regionManager = regionManager;
+            DecreaseStepperCommand = new DelegateCommand(OnDecreaseStepper, CanDecreaseStepper);
+            IncreaseStepperCommand = new DelegateCommand(OnIncreaseStepper);
+            AddToCartCommand = new DelegateCommand(OnAddToCart);
+        }
+
+        private bool CanDecreaseStepper()
+        {
+            if (Stepper < 2)
+                return false;
+
+            return true;
+        }
+
+        private void OnAddToCart()
+        {
+            _regionManager.RequestNavigate(Regions.MainRegion, ViewsNames.ProductsView);
+        }
+
+        private void OnIncreaseStepper()
+        {
+            Stepper++;
+        }
+
+        private void OnDecreaseStepper()
+        {
+            Stepper--;
+        }
+
+        public DelegateCommand DecreaseStepperCommand { get; set; }
+        public DelegateCommand IncreaseStepperCommand { get; set; }
+        public DelegateCommand AddToCartCommand { get; set; }
 
         public bool IsNavigationTarget(NavigationContext navigationContext)
         {
@@ -27,7 +79,13 @@ namespace ECommerce.Main.ViewModels
         public void OnNavigatedTo(NavigationContext navigationContext)
         {
             if (navigationContext.Parameters.ContainsKey("productId"))
-                ProductId = navigationContext.Parameters.GetValue<int>("productId");
+            {
+                var productId = navigationContext.Parameters.GetValue<int>("productId");
+
+                Product = _productService.GetProductById(productId);
+
+                Stepper = 1;
+            }
         }
     }
 }
